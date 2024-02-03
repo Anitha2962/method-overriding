d57c4968df7e120396bf463109f89d21f2601362
anitha2962 <anithasivadas92@gmail.com>
2024-02-03 20:18:44 +0530

    'first-commit'

---
+++ METHOD OVERRIDING/Program.cs
@@ -0,0 +1,27 @@
+﻿
+class Animal
+{
+    public virtual void animalsound()
+    {
+        Console.WriteLine("the animal make a sound");
+    }
+    
+}
+class pig:Animal
+{
+    public override void animalsound()
+    {
+        Console.WriteLine("the pig says wee weee");
+    }
+}
+
+class program
+{
+    static void main(string[] args)
+    {
+        Animal myanimal = new Animal();
+        Animal mypig= new pig();
+        myanimal.animalsound();
+        mypig.animalsound();
+    }
+}
